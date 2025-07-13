d46be7a2e6cc08f32d806e8c1c44e57479a88407
Bhagyasreej <159531398+Bhagyasreej@users.noreply.github.com>
2025-07-13 19:39:43 +0530

    Update SampleprojectApplication.java
    commit

--- src/main/java/com/example/demo/SampleprojectApplication.java
+++ src/main/java/com/example/demo/SampleprojectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleprojectApplication {
+	// comment for testings
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleprojectApplication.class, args);
