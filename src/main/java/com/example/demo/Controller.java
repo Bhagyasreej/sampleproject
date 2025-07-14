0fa9a3de9b8ca5ff0aef2aba6ed7c18648346d41
Bhagya Sree Jarugu <jbsree019@gmail.com>
2025-07-14 19:21:03 +0530

    taskid #1 added the setter and getters

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -7,4 +7,21 @@ public class Controller {
   
   private String name="name";
 
+  public int getA() {
+	return a;
+  }
+
+  public void setA(int a) {
+	this.a = a;
+  }
+
+  public String getName() {
+	return name;
+  }
+
+  public void setName(String name) {
+	this.name = name;
+  }
+  
+
 }
